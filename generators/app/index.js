9305b296ee4d9b27edbba82c562ad852915d0446
Dongyi An <acmilank22@gmail.com>
2019-07-26 14:09:51 +0800

    update

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,62 +1,11 @@
 'use strict';
 const Generator = require('yeoman-generator');
 
+const { base, eslint } = require('./questions');
+
 module.exports = class extends Generator {
   prompting() {
-    const prompts = [
-      {
-        type: 'checkbox',
-        name: 'config',
-        message: 'Choose Config',
-        choices: [
-          {
-            name: 'EditorConfig',
-            value: 'editorconfig',
-            checked: true
-          },
-          {
-            name: 'Prettier',
-            value: 'prettier',
-            checked: true
-          },
-          {
-            name: 'ESLint',
-            value: 'eslint',
-            checked: true
-          },
-          {
-            name: 'stylelint',
-            value: 'stylelint',
-            checked: true
-          },
-          {
-            name: 'lint-staged',
-            value: 'lint-staged',
-            checked: true
-          },
-          {
-            name: 'HTMLHint',
-            value: 'htmlhint',
-            checked: false
-          },
-          {
-            name: '.gitignore',
-            value: 'gitignore',
-            checked: true
-          },
-          {
-            name: '.gitattributes',
-            value: 'gitattributes',
-            checked: true
-          },
-          {
-            name: 'LICENSE(MIT)',
-            value: 'license',
-            checked: true
-          }
-        ]
-      }
-    ];
+    const prompts = [base, eslint];
 
     return this.prompt(prompts).then(props => {
       this.log(props);
