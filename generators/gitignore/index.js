121141fd4c2141906e7f6a5e7d7242872bea3fb5
Dongyi An <acmilank22@gmail.com>
2019-08-18 15:13:10 +0800

    update

--- generators/gitignore/index.js
+++ generators/gitignore/index.js
@@ -2,10 +2,30 @@
 
 const Generator = require('yeoman-generator');
 
+const request = require('../../utils/request');
+
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
   }
 
-  writing() {}
+  async writing() {
+    const url =
+      'https://www.gitignore.io/api/code,flutter,grunt,jetbrains,jetbrains+all,macos,node,osx,reactnative,sass,visualstudiocode,windows';
+    const fileName = '.gitignore';
+    const res = await request(url);
+    if (typeof res === 'string') {
+      const custom = [
+        '# Custom',
+        'build/',
+        'dist/',
+        'miniprogram_npm/',
+        '!bin/',
+        '!.env',
+        '!.env.test'
+      ].join('\n');
+      const contents = [res, '\n', custom, '\n'].join('');
+      this.fs.write(this.destinationPath(fileName), contents);
+    }
+  }
 };
