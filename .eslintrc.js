365c0805fae14cde0f38a8b7bbc14b9d61328f89
Donnie An <acmilank22@gmail.com>
2020-09-09 11:48:43 +0800

    feat: updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,56 @@
 module.exports = {
-  plugins: ['html'],
-  parserOptions: { parser: 'babel-eslint' },
   extends: [
     'airbnb-base',
     'plugin:node/recommended',
     'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    'shared-node-browser': true,
+    amd: true,
+    es6: true,
+    es2017: true,
+    es2020: true,
+  },
+  plugins: ['html'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'node/no-missing-import': 'off',
+    'node/no-unsupported-features/es-builtins': [
+      'error',
+      { version: '>=12.0.0', ignores: [] },
+    ],
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { version: '>=12.0.0', ignores: ['modules'] },
+    ],
+    'node/no-unsupported-features/node-builtins': [
+      'error',
+      { version: '>=12.0.0', ignores: [] },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        'webpack.js',
+        'webpack.*.js',
+        'rollup.js',
+        'rollup.*.js',
+        'gulpfile.js',
+        'gulpfile.*.js',
+        'postcss.config.js',
+        'postcss.*.js',
+      ],
+      rules: {
+        'node/no-unpublished-import': 'off',
+        'node/no-unpublished-require': 'off',
+      },
+    },
   ],
 };
