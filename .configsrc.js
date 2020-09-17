2251788465858c2b19fcf2bc687340187e9a316f
Donnie An <acmilank22@gmail.com>
2020-09-17 12:20:29 +0800

    fix: bugs

---
+++ .configsrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  editorconfig: true,
+  prettier: true,
+  eslint: [true, { es6: true }],
+  cspell: true,
+  commitlint: true,
+  'lint-staged': true,
+  gitignore: true,
+  gitattributes: true,
+  license: true,
+};
