4264a74e9579cac16e6a8240450a5aa28dd631ad
Donnie An <acmilank22@gmail.com>
2020-09-23 14:56:06 +0800

    feat: add languages

--- .configsrc.js
+++ .configsrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  editorconfig: true,
-  prettier: true,
-  eslint: [true, { preset: 'es6' }],
-  cspell: true,
-  commitlint: true,
-  'lint-staged': true,
-  gitignore: true,
-  gitattributes: true,
-  license: true,
+  languages: ['js', 'css', 'scss'],
+  configs: [
+    'editorconfig',
+    'prettier',
+    'eslint',
+    'stylelint',
+    'htmlhint',
+    'commitlint',
+  ],
 };
