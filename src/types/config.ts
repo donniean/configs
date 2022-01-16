8dba5fa97fcd21e23e1812fa67f24778afeb08f8
donniean <acmilank22@gmail.com>
2022-01-16 20:51:25 +0800

    feat: stylelint template

--- src/types/config.ts
+++ src/types/config.ts
@@ -20,37 +20,47 @@ export type PrettierExtension =
 
 export type ESLintExtension = 'js' | 'jsx' | 'ts' | 'tsx' | 'mjs' | 'cjs';
 
+export type StylelintExtension =
+  | 'css'
+  | 'less'
+  | 'scss'
+  | 'js'
+  | 'jsx'
+  | 'ts'
+  | 'tsx';
+
 export interface Config {
-  modules: {
-    editorconfig: boolean;
-    prettier: {
-      extensions: PrettierExtension[];
-      ignore: string[];
+  modules?: {
+    editorconfig?: boolean;
+    prettier?: {
+      extensions?: PrettierExtension[];
+      ignore?: string[];
     };
-    eslint: {
-      extensions: ESLintExtension[];
-      presets: [];
-      ignore: [];
+    eslint?: {
+      extensions?: ESLintExtension[];
+      presets?: string[];
+      ignore?: string[];
     };
-    stylelint: {
-      extensions: [];
-      ignore: [];
+    stylelint?: {
+      extensions?: StylelintExtension[];
+      ignore?: string[];
     };
-    htmlhint: true;
-    cspell: {
-      extensions: ['*'];
-      ignore: [];
+    htmlhint?: boolean;
+    cspell?: {
+      extensions?: ['*'];
+      ignore?: string[];
     };
-    commitlint: true;
-    'lint-staged': {
-      prettier: true;
-      eslint: true;
-      stylelint: true;
-      cspell: true;
+    commitlint?: boolean;
+    'lint-staged'?: {
+      prettier?: boolean;
+      eslint?: boolean;
+      tsc?: boolean;
+      stylelint?: boolean;
+      cspell?: boolean;
     };
-    husky: true;
-    gitignore: true;
-    gitattributes: true;
+    husky?: boolean;
+    gitignore?: boolean;
+    gitattributes?: boolean;
   };
-  ignore: [];
+  ignore?: string[];
 }
