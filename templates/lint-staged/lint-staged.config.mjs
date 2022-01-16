2658d93c1adf82041c5d7fa72abdd5650861e0bf
donniean <acmilank22@gmail.com>
2022-01-16 14:07:07 +0800

    feat: lint-staged template

---
+++ templates/lint-staged/lint-staged.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  '*.{js,jsx,ts,tsx,mjs,cjs,json,html,vue,hbs,handlebars,css,less,scss,md,mdx,yaml,yml}':
+    'prettier --write',
+  '*.{js,jsx,ts,tsx,mjs,cjs,vue}': 'eslint --fix',
+  '*.{ts,tsx}': 'tsc --noEmit',
+  '*.{css,less,scss,js,jsx,ts,tsx,html,vue}': 'stylelint --fix',
+  '*.html': 'htmlhint',
+  '**': 'cspell --no-must-find-files',
+};
