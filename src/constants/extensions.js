d739107520e81221bf53cd782536bd6815d640e0
donniean <acmilank22@gmail.com>
2022-04-02 18:34:23 +0800

    feat: logger

---
+++ src/constants/extensions.js
@@ -0,0 +1,38 @@
+'use strict';
+exports.__esModule = true;
+exports.STYLELINT_EXTENSIONS =
+  exports.ESLINT_EXTENSIONS =
+  exports.TSC_EXTENSIONS =
+  exports.PRETTIER_EXTENSIONS =
+    void 0;
+exports.PRETTIER_EXTENSIONS = [
+  'js',
+  'jsx',
+  'ts',
+  'tsx',
+  'mjs',
+  'cjs',
+  'json',
+  'html',
+  'vue',
+  'hbs',
+  'handlebars',
+  'css',
+  'less',
+  'scss',
+  'md',
+  'mdx',
+  'yaml',
+  'yml',
+];
+exports.TSC_EXTENSIONS = ['ts', 'tsx'];
+exports.ESLINT_EXTENSIONS = ['js', 'jsx', 'ts', 'tsx', 'mjs', 'cjs'];
+exports.STYLELINT_EXTENSIONS = [
+  'css',
+  'less',
+  'scss',
+  'js',
+  'jsx',
+  'ts',
+  'tsx',
+];
---
+++ src/index.js
@@ -0,0 +1,4 @@
+'use strict';
+exports.__esModule = true;
+var cli_1 = require('@/cli');
+(0, cli_1['default'])();
