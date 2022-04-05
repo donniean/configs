4e6763078c758f5fd2770a7e9cc43a252d1e4cb4
donniean <acmilank22@gmail.com>
2022-04-05 22:32:55 +0800

    feat: logger

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,5 @@
 import logger from '@/utils/logger';
+import paths from '@/utils/paths';
 
 import commands from './commands';
 
@@ -6,4 +7,6 @@ export default function cli() {
   const argv = commands();
 
   logger.log(argv);
+
+  logger.log(paths);
 }
---
+++ src/utils/paths/index.ts
@@ -0,0 +1,8 @@
+// import path from 'node:path';
+
+const paths = {
+  cwd: process.cwd(),
+  dirname: __dirname,
+};
+
+export default paths;
