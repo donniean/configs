78a00a5645469dafc67279a5df6d4d1b24aa946f
donniean <acmilank22@gmail.com>
2022-07-04 18:30:38 +0800

    feat: m

--- src/templates/lint-staged/lint-staged.config.js
+++ src/templates/lint-staged/lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '*.{js,jsx,ts,tsx,mjs,cjs,json,html,vue,hbs,handlebars,css,less,scss,md,mdx,yaml,yml}':
     'prettier --write',
   '*.{js,jsx,ts,tsx,mjs,cjs,vue}': 'eslint --fix',
-  '*.{ts,tsx}': 'tsc --noEmit',
+  '*.{ts,tsx}': 'bash -c tsc --noEmit',
   '*.{css,less,scss,js,jsx,ts,tsx,html,vue}': 'stylelint --fix',
   '*.html': 'htmlhint',
   '*.md': 'markdownlint --fix',
