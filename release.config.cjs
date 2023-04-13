8822fd64c016323beb7889d84bd4427731a4b006
donniean <acmilank22@gmail.com>
2023-04-13 13:35:17 +0800

    ci: modify release.config.cjs (#34)
    * feat: add @semantic-release/git
    
    * ci: modify release.config.cjs

--- release.config.cjs
+++ release.config.cjs
@@ -3,15 +3,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json'],
-        message:
-          // eslint-disable-next-line no-template-curly-in-string
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
     '@semantic-release/npm',
+    '@semantic-release/git',
   ],
 };
