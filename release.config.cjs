b10421f0f389efd7a1252d3a2170e4c769fd311f
donniean <acmilank22@gmail.com>
2023-04-16 02:26:49 +0800

    ci: place @semantic-release/exec before @semantic-release/git in semantic-release config (#75)

--- release.config.cjs
+++ release.config.cjs
@@ -6,12 +6,12 @@ module.exports = {
     '@semantic-release/changelog',
     // '@semantic-release/npm',
     '@semantic-release/github',
-    '@semantic-release/git',
     [
       '@semantic-release/exec',
       {
         prepareCmd: 'npx prettier --write "CHANGELOG.md"',
       },
     ],
+    '@semantic-release/git',
   ],
 };
