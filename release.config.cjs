80c149b30ad28e66e3c4b5e022073da7de1477e6
donniean <acmilank22@gmail.com>
2023-04-15 21:48:54 +0800

    chore: modify version to 5.0.0

--- release.config.cjs
+++ release.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-  branches: ['main', 'release'],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
+    // '@semantic-release/npm',
     '@semantic-release/github',
     '@semantic-release/git',
   ],
