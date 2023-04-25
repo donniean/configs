3fe9ef18e1f3167ce1016d1307b8198d4b9f851c
donniean <acmilank22@gmail.com>
2023-04-25 21:57:23 +0800

    feat: eslint rules

--- src/features/eslint/rules/airbnb-base.ts
+++ src/features/eslint/rules/airbnb-base.ts
@@ -1,8 +1,39 @@
+import type { Linter } from 'eslint';
+
 const devDependencies = {
   'eslint-config-airbnb-base': '',
   'eslint-plugin-import': '',
 };
 
-const config = {};
+const config: Linter.Config = {
+  extends: ['airbnb-base'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.{mjs,cjs}'] },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'object',
+          'type',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          orderImportKind: 'asc',
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+};
 
 export { config, devDependencies };
