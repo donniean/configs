061081ff738bada698d00e9fd913f55eb92d5c01
donniean <acmilank22@gmail.com>
2023-05-08 16:20:39 +0800

    test: add test

--- src/features/eslint/utils.test.ts
+++ src/features/eslint/utils.test.ts
@@ -10,8 +10,19 @@ describe('test hasTypeScriptFn', () => {
       },
     },
   };
-  test('js and ts', () => {
+
+  test('js pattern', () => {
+    normalizedConfigsConfig.features.eslint.patterns = ['**/*.js'];
+    expect(hasTypeScriptFn(normalizedConfigsConfig)).toBe(false);
+  });
+
+  test('js and ts pattern', () => {
     normalizedConfigsConfig.features.eslint.patterns = ['**/*.{js,ts}'];
+    expect(hasTypeScriptFn(normalizedConfigsConfig)).toBe(true);
+  });
+
+  test('js pattern and ts pattern', () => {
+    normalizedConfigsConfig.features.eslint.patterns = ['**/*.js', '**/*.ts'];
     expect(hasTypeScriptFn(normalizedConfigsConfig)).toBe(true);
   });
 });
