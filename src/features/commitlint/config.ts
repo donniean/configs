2c9940fff7fef6af29b3eba17e00c4a87622e564
donniean <acmilank22@gmail.com>
2024-01-26 01:07:25 +0800

    chore: commit lint use ESM config file

--- src/features/commitlint/config.ts
+++ src/features/commitlint/config.ts
@@ -2,8 +2,8 @@ import type { FeatureConfig } from '@/types/feature-configs';
 
 export function getConfig(): FeatureConfig<{ extends: string[] }> {
   return {
-    outputFileName: 'commitlint.config.cjs',
-    format: 'cjs',
+    outputFileName: 'commitlint.config.mjs',
+    format: 'esm',
     data: {
       extends: ['@commitlint/config-conventional'],
     },
