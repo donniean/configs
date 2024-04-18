038f0ec248f5831cc1119adbc81401ee95ccf265
donniean <acmilank22@gmail.com>
2024-04-18 13:53:16 +0800

    chore: delete AutoCorrect (#697)
    Signed-off-by: donniean <acmilank22@gmail.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,7 +2,6 @@ export default {
   'package.json': 'sort-package-json',
   '*': [
     'prettier --write --ignore-unknown',
-    'autocorrect --fix',
     'cspell lint --no-progress --relative --no-must-find-files --dot --gitignore',
   ],
   '*.ts': ['bash -c tsc --noEmit', 'vitest related --run'],
