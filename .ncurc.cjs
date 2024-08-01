6244202281af6ed2378558e873b08b1387eedded
donniean <donniean1@gmail.com>
2024-08-01 10:18:29 +0800

    chore: update .ncurc.cjs
    Signed-off-by: donniean <donniean1@gmail.com>

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   reject: [
     '@types/eslint',
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint/parser',
     'eslint',
     'eslint-plugin-sonarjs',
     'eslint-plugin-vitest',
