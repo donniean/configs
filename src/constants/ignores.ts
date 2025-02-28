ab92f606dc1f015ec93e4f49afd001ce0badbedc
donniean <donniean1@gmail.com>
2025-03-01 01:43:25 +0800

    chore: update ignore patterns in linter and attributes files
    Signed-off-by: donniean <donniean1@gmail.com>

--- src/constants/ignores.ts
+++ src/constants/ignores.ts
@@ -1,29 +1,29 @@
 const LINTER_IGNORE = [
   '.git/',
   '.history/',
-  'node_modules/',
-  'coverage/',
-  'dist/',
-  '.next/',
+  '**/node_modules/',
+  '**/coverage/',
+  '**/dist/',
+  '**/.next/',
   '**/*.min.*',
 ];
 
-const CHANGELOG_IGNORE = ['CHANGELOG.md'];
+const CHANGELOG_IGNORE = ['**/CHANGELOG.md'];
 
 export const CSPELL_IGNORE = [
   ...LINTER_IGNORE,
   '.idea/',
   '.vscode/',
-  '**/*.svg',
   '.*ignore',
-  '*.tsbuildinfo',
   '.gitattributes',
-  '.htmlhintrc',
-  'package.json',
-  'package-lock.json',
-  'yarn.lock',
-  'pnpm-lock.yaml',
-  'Dockerfile',
+  '**/.htmlhintrc',
+  '**/*.tsbuildinfo',
+  '**/*.svg',
+  '**/package.json',
+  '**/package-lock.json',
+  '**/yarn.lock',
+  '**/pnpm-lock.yaml',
+  '**/Dockerfile',
   ...CHANGELOG_IGNORE,
 ];
 
@@ -35,7 +35,7 @@ export const MARKDOWNLINT_IGNORE = [
   ...CHANGELOG_IGNORE,
 ];
 
-export const PRETTIER_IGNORE = [...LINTER_IGNORE, '*.tsbuildinfo'];
+export const PRETTIER_IGNORE = [...LINTER_IGNORE, '**/*.tsbuildinfo'];
 
 export const AUTOCORRECT_IGNORE = [...LINTER_IGNORE, ...CHANGELOG_IGNORE];
 
