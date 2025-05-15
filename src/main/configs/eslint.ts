fa877965e8d5c2629a7bf052fbbf60e3abc82bb8
donniean <donniean1@gmail.com>
2025-05-15 16:17:31 +0800

    feat: update ESLint configuration with dependencies and scripts
    Signed-off-by: donniean <donniean1@gmail.com>

--- src/main/configs/eslint.ts
+++ src/main/configs/eslint.ts
@@ -1,15 +1,39 @@
 import type { Config } from '../types';
 
 export const CONFIG = {
-  name: '',
-  url: '',
-  devDependencies: [''],
-  filePaths: [],
+  name: 'ESLint',
+  url: 'https://github.com/eslint/eslint',
+  devDependencies: [
+    '@eslint-community/eslint-plugin-eslint-comments',
+    '@eslint/js',
+    '@tanstack/eslint-plugin-query',
+    '@vitest/eslint-plugin',
+    'eslint',
+    'eslint-config-prettier',
+    'eslint-import-resolver-typescript',
+    'eslint-plugin-import-x',
+    'eslint-plugin-jsx-a11y',
+    'eslint-plugin-n',
+    'eslint-plugin-promise',
+    'eslint-plugin-react',
+    'eslint-plugin-react-hooks',
+    'eslint-plugin-react-refresh',
+    'eslint-plugin-simple-import-sort',
+    'eslint-plugin-sonarjs',
+    'eslint-plugin-unicorn',
+    'eslint-plugin-unused-imports',
+    'globals',
+    'typescript-eslint',
+  ],
+  filePaths: ['eslint.config.mjs'],
   install: [
     { type: 'devDependencies.install' },
     {
       type: 'packageJson.set',
-      values: [''],
+      values: [
+        'scripts.lint:js="eslint **/*.{js,mjs,cjs,ts,tsx}"',
+        'scripts.lint:js:fix="npm run lint:js -- --fix',
+      ],
     },
     {
       type: 'files.download',
@@ -19,7 +43,7 @@ export const CONFIG = {
     { type: 'devDependencies.uninstall' },
     {
       type: 'packageJson.delete',
-      values: [''],
+      values: ['scripts.lint:js', 'scripts.lint:js:fix'],
     },
     {
       type: 'files.delete',
--- src/main/configs/index.ts
+++ src/main/configs/index.ts
@@ -3,6 +3,7 @@ import { CONFIG as commitizen } from './commitizen';
 import { CONFIG as commitlint } from './commitlint';
 import { CONFIG as cspell } from './cspell';
 import { CONFIG as editorconfig } from './editorconfig';
+import { CONFIG as eslint } from './eslint';
 
 export const CONFIGS = [
   autocorrect,
@@ -10,4 +11,5 @@ export const CONFIGS = [
   commitlint,
   cspell,
   editorconfig,
+  eslint,
 ] as const;
