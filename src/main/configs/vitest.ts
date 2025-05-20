cb0e5526808b7336d61a308cc741eb3c3d357697
donniean <donniean1@gmail.com>
2025-05-21 01:53:38 +0800

    fix: remove fetch-depth from checkout step in CI workflow
    Signed-off-by: donniean <donniean1@gmail.com>

--- src/main/configs/vitest.ts
+++ src/main/configs/vitest.ts
@@ -4,11 +4,12 @@ export const CONFIG = {
   name: 'Vitest',
   url: 'https://github.com/vitest-dev/vitest',
   pkg: {
-    devDependencies: ['@vitest/coverage-v8', 'vitest'],
+    devDependencies: ['@vitest/coverage-v8', '@vitest/ui', 'vitest'],
     scripts: [
       { key: 'test', value: 'vitest run' },
       { key: 'test:coverage', value: 'vitest run --coverage' },
       { key: 'test:watch', value: 'vitest watch' },
+      { key: 'test:ui', value: 'vitest --ui' },
     ],
   },
   install: [
