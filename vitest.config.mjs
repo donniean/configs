f64110f5d8cce91b336e6113d9127884ce1ce6ff
donniean <12584040+donniean@users.noreply.github.com>
2025-06-04 22:13:25 +0800

    chore: simplify dirname extraction in vitest config (#1324)
    Signed-off-by: donniean <donniean1@gmail.com>

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,9 +1,8 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vitest/config';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
+const { dirname } = import.meta;
 
 export default defineConfig({
   test: {
