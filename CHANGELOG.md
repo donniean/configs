33cc2201c28131417e141367516b7dbbd5dbb6b2
donniean <donniean1@gmail.com>
2025-06-07 22:22:05 +0800

    fix: update project name in README and API to match repository name
    Signed-off-by: donniean <donniean1@gmail.com>

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @donniean/node-app
-
-## 0.0.0
-
-### Patch Changes
-
-- [#1319](https://github.com/donniean/node-app/pull/1319) [`1acd61f`](https://github.com/donniean/node-app/commit/1acd61f19427dfca7b4279f3a91186c512dbb1c0) Thanks [@donniean](https://github.com/donniean)! - chore: test
--- src/api.ts
+++ src/api.ts
@@ -60,7 +60,7 @@ async function getMarkdown(configs: Configs) {
   }
 
   const data = [
-    { h1: '@donniean/node-app' },
+    { h1: 'node-app' },
     {
       p: '[![Version](https://img.shields.io/npm/v/@donniean/node-app.svg)](https://www.npmjs.com/package/@donniean/node-app) [![License: MIT](https://img.shields.io/github/license/donniean/node-app)](https://github.com/donniean/node-app/blob/master/LICENSE) [![CI](https://github.com/donniean/node-app/actions/workflows/ci.yaml/badge.svg)](https://github.com/donniean/node-app/actions/workflows/ci.yaml) [![Release](https://github.com/donniean/node-app/actions/workflows/release.yaml/badge.svg)](https://github.com/donniean/node-app/actions/workflows/release.yaml)',
     },
